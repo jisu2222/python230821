26d818563a5f9da9ce5aefc97976beefc5dff12b
jisu2222 <tttop26@naver.com>
2023-08-21 16:48:06 +0900

    약간수정

---
+++ function2.py
@@ -0,0 +1,35 @@
+# function2.py
+#스코핑룰(이름 해석 규칙):LGB규칙 
+#전역변수
+X = 1
+def func(a):
+    return a+X
+
+#호출
+print(func(1))
+
+def func2(a):
+    #지역변수
+    X = 5
+    return a+X
+
+#호출
+print(func2(1))
+
+#기본값이 있는 함수
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드 인자 방식
+def connectURI(server, port):
+    strURL = "https://" + server + ":" + port
+    return strURL
+
+#호출
+print(connectURI("multicampus.com","80"))
+print(connectURI(port="80", server="multicampus.com"))
